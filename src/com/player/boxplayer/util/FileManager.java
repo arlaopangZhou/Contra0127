7cbf90f0930aeeb73797724d8f7ed41eecccb8c5
arlaopangZhou <richardchongkung@gmail.com>
2014-01-26 18:22:28 -0800

    文件管理

---
+++ src/com/player/boxplayer/util/FileManager.java
@@ -0,0 +1,19 @@
+package com.player.boxplayer.util;
+/**
+ * 文件管理接口，包括名称以及路径等。可根据需要添一些操作方法。
+ * @author richardzhou
+ *
+ */
+public interface FileManager {
+
+	/**
+	 * 文件的保存载SD卡上的路径
+	 */
+	public static String FILEDIR = "";
+	
+	/**
+	 * 保存载SD卡上的文件名
+	 */
+	public static String FILENAME = "contra.xml";
+
+}
